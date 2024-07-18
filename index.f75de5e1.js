3b727aac233e3b22f97cc1cb53f11c1dc8d9f6ce
Serhii Babskyi <sergbald99@gmail.com>
2024-07-18 21:56:48 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const populations = [
+    ...document.querySelectorAll(".population")
+].map((el)=>+el.textContent.replaceAll(",", ""));
+const total = populations.reduce((sum, num)=>sum + num, 0);
+const average = total / populations.length;
+document.querySelector(".total-population").textContent = addSeparator(total);
+document.querySelector(".average-population").textContent = addSeparator(average);
+function addSeparator(num) {
+    return num.toLocaleString("en-US");
+}
+
+//# sourceMappingURL=index.f75de5e1.js.map
